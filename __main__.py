8034b550d50efb86bbd379525bd48dcffc132157
The brain <colbyatcheson81@gmail.com>
2025-07-29 07:58:22 -0600

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -32,3 +32,15 @@ def main():
 
 if __name__ == "__main__":
     main()
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'MandemOS Hecate online.'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # 👈 uses Railway's port
+    app.run(host='0.0.0.0', port=port)
